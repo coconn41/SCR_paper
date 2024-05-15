9b1b5adbc261464e1110fdf101a2b9770ae5c968
coconn41 <collinoconnor3@gmail.com>
2024-05-15 13:44:58 -0400

    Updates to roadway data and upload first of 11

--- Scripts/SCR/Roadway_data_cleaning.R
+++ Scripts/SCR/Roadway_data_cleaning.R
@@ -1,12 +1,22 @@
 minor_roadways = c(7,8,9,17,18,19)
 major_roadways = c(1,2,4,6,11,12,14,16)
 
-roadwaydat = read_sf(paste0(getwd(),
-                            "/LCP_analysis/Input_data/NYSDOT Functional Class 07_10_2023/NYSDOT_Functional_Class_07_10_2023.shp")) %>%
-  st_zm() %>%
-  mutate(Categorization = ifelse(FUNCTIONAL %in% minor_roadways,1,
-                                 ifelse(FUNCTIONAL %in% major_roadways,2,0))) %>%
-  st_transform(.,crs=st_crs(NYS))
+for(i in 1:11){
+  rwd=read_sf(paste0(getwd(),'/Data/Input_data/Roadway_data/Roadway_data',i,".shp"))
+  if(i==1){rwd2=rwd}
+  if(i>1){rwd2 = rbind(rwd,rwd2)}
+}
+
+roadwaydat = rwd2 %>%
+  mutate(Categorization = Ctgrztn) %>%
+  st_transform(.,crs=st_crs(template))
+remove(rwd,rwd2)
+# roadwaydat = read_sf(paste0(getwd(),
+#                             "/Data/Input_data/NYSDOT Functional Class 07_10_2023/NYSDOT_Functional_Class_07_10_2023.shp")) %>%
+#   st_zm() %>%
+#   mutate(Categorization = ifelse(FUNCTIONAL %in% minor_roadways,1,
+#                                  ifelse(FUNCTIONAL %in% major_roadways,2,0))) %>%
+#   st_transform(.,crs=st_crs(template))
 
 road_vect = terra::vect(roadwaydat)
 road_pix  = terra::rasterize(x = road_vect,
