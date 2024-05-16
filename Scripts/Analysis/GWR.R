a279d694a8ea8097250bc580ecd6c195c7498489
coconn41 <collinoconnor3@gmail.com>
2024-05-16 15:47:26 -0400

    Upload GWR maps and code

---
+++ Scripts/Analysis/GWR.R
@@ -0,0 +1,111 @@
+#####
+# Load regression/analysis datasets:
+#####
+source(paste0(getwd(),'/Scripts/Analysis/Stat_dataset_compile.R'))
+
+#####
+# Run GWR and get average parameters for all years
+#####
+library(hgwrr)
+library(spgwr)
+
+spatial_reg_dat = spatial_reg_dat %>%
+  mutate(longitude = st_coordinates(st_centroid(spatial_reg_dat))[,1],
+         latitude = st_coordinates(st_centroid(spatial_reg_dat))[,2])
+for(i in min(spatial_reg_dat$huntyear):max(spatial_reg_dat$huntyear)){
+  custom_gwr_dat = spatial_reg_dat %>%
+    filter(index=="custom_LS_metric",
+           wmu_type=="unbuffered",
+           huntyear==i)
+  PC_gwr_dat = spatial_reg_dat %>%
+    filter(index=="PC_index_double",
+           wmu_type=="unbuffered",
+           huntyear==i)
+  ECA_gwr_dat = spatial_reg_dat %>%
+    filter(index=="ECA_double",
+           wmu_type=="unbuffered",
+           huntyear==i)
+  
+  fm = log(Density) ~ value
+  custom_band_width=spgwr::gwr.sel(formula = fm,
+                                   data = custom_gwr_dat,
+                                   coords = cbind(custom_gwr_dat$longitude,
+                                                  custom_gwr_dat$latitude),
+                                   adapt=F,
+                                   verbose = F)
+  
+  
+  custom_gwr_mod=spgwr::gwr(formula = fm,
+                            data = custom_gwr_dat,
+                            bandwidth=custom_band_width,
+                            coords = cbind(custom_gwr_dat$longitude,
+                                           custom_gwr_dat$latitude))
+  PC_band_width=spgwr::gwr.sel(formula = fm,
+                               data = PC_gwr_dat,
+                               coords = cbind(PC_gwr_dat$longitude,
+                                              PC_gwr_dat$latitude),
+                               adapt=F,
+                               verbose = F)
+  
+  
+  PC_gwr_mod=spgwr::gwr(formula = fm,
+                        data = PC_gwr_dat,
+                        bandwidth=PC_band_width,
+                        coords = cbind(PC_gwr_dat$longitude,
+                                       PC_gwr_dat$latitude))
+  ECA_band_width=spgwr::gwr.sel(formula = fm,
+                                data = ECA_gwr_dat,
+                                coords = cbind(ECA_gwr_dat$longitude,
+                                               ECA_gwr_dat$latitude),
+                                adapt=F,
+                                verbose = F)
+  
+  
+  ECA_gwr_mod=spgwr::gwr(formula = fm,
+                         data = ECA_gwr_dat,
+                         bandwidth=ECA_band_width,
+                         coords = cbind(ECA_gwr_dat$longitude,
+                                        ECA_gwr_dat$latitude))
+  
+  
+  
+  
+  if(i==min(spatial_reg_dat$huntyear)){
+    gwr_plot_dat = spatial_reg_dat %>%
+      filter(index=="custom_LS_metric",
+             wmu_type=="unbuffered",
+             huntyear==i) %>%
+      mutate(params = custom_gwr_mod$SDF$value) %>%
+      bind_rows(.,spatial_reg_dat %>%
+                  filter(index=="PC_index_double",
+                         wmu_type=="unbuffered",
+                         huntyear==i) %>%
+                  mutate(params = PC_gwr_mod$SDF$value)) %>%
+      bind_rows(.,spatial_reg_dat %>%
+                  filter(index=="ECA_double",
+                         wmu_type=="unbuffered",
+                         huntyear==i) %>%
+                  mutate(params = ECA_gwr_mod$SDF$value))
+  }
+  if(i>min(spatial_reg_dat$huntyear)){
+    gwr_plot_dat2 = spatial_reg_dat %>%
+      filter(index=="custom_LS_metric",
+             wmu_type=="unbuffered",
+             huntyear==i) %>%
+      mutate(params = custom_gwr_mod$SDF$value) %>%
+      bind_rows(.,spatial_reg_dat %>%
+                  filter(index=="PC_index_double",
+                         wmu_type=="unbuffered",
+                         huntyear==i) %>%
+                  mutate(params = PC_gwr_mod$SDF$value)) %>%
+      bind_rows(.,spatial_reg_dat %>%
+                  filter(index=="ECA_double",
+                         wmu_type=="unbuffered",
+                         huntyear==i) %>%
+                  mutate(params = ECA_gwr_mod$SDF$value))
+    gwr_plot_dat = rbind(gwr_plot_dat2,gwr_plot_dat)}
+}
+
+mean_params = gwr_plot_dat %>%
+  group_by(UNIT,index) %>%
+  summarize(mn_params = mean(params,na.rm=T)) 
---
+++ Scripts/Figures/GWR_map.R
@@ -0,0 +1,158 @@
+#####
+# Run GWR script:
+#####
+source(paste0(getwd(),'/Scripts/Analysis/GWR.R'))
+
+#####
+# Generate SCR GWR map:
+######
+custom_params = mean_params %>%
+  filter(index=='custom_LS_metric') %>%
+  mutate(colors = case_when(mn_params < -4 ~ "< -4",
+                            mn_params >= -4 &
+                              mn_params < -3 ~ "-4 to -3",
+                            mn_params >= -3 &
+                              mn_params < -2 ~ "-3 to -2",
+                            mn_params >= -2 &
+                              mn_params < -1 ~ "-2 to -1",
+                            mn_params >= -1 & 
+                              mn_params < 0 ~ "-1 to 0",
+                            # mn_params == 0 ~ "0",
+                            mn_params > 0 & 
+                              mn_params < 1 ~ "0 to 1",
+                            mn_params >= 1 &
+                              mn_params < 2 ~ "1 to 2",
+                            mn_params >= 2 &
+                              mn_params < 3 ~ "2 to 3",
+                            mn_params >= 3 &
+                              mn_params ~ "3 to 4",
+                            mn_params >= 4 ~ "> 4")) %>%
+  mutate(colors = factor(colors,
+                         levels=c("< -4",
+                                  "-4 to -3",
+                                  "-3 to -2",
+                                  "-2 to -1",
+                                  "-1 to 0",
+                                  #  "0",
+                                  "0 to 1",
+                                  "1 to 2",
+                                  "2 to 3",
+                                  "3 to 4",
+                                  "> 4")))
+
+custom_parameter_map=tm_shape(custom_params)+
+  tm_polygons(col='colors',
+              palette=tmaptools::get_brewer_pal("PRGn",n=10),
+              title="Sinuous Connection Reduction")+
+  tm_layout(main.title="C",
+            legend.title.size = 1.5,
+            legend.text.size = .9)
+
+#####
+# Generate PC GWR map:
+#####
+PC_params = mean_params %>%
+  filter(index=="PC_index_double") %>%
+  mutate(colors = case_when(mn_params < -4 ~ "< -4",
+                            mn_params >= -4 &
+                              mn_params < -3 ~ "-4 to -3",
+                            mn_params >= -3 &
+                              mn_params < -2 ~ "-3 to -2",
+                            mn_params >= -2 &
+                              mn_params < -1 ~ "-2 to -1",
+                            mn_params >= -1 &
+                              mn_params < 0 ~ "-1 to 0",
+                            mn_params >= 0 & 
+                              mn_params < 1 ~ "0 to 1",
+                            mn_params >= 1 &
+                              mn_params < 2 ~ "1 to 2",
+                            mn_params >= 2 &
+                              mn_params < 3 ~ "2 to 3",
+                            mn_params >= 3 &
+                              mn_params < 4 ~ "3 to 4",
+                            mn_params >= 4 ~ "> 4")) %>%
+  mutate(colors = factor(colors,
+                         levels=c("< -4",
+                                  "-4 to -3",
+                                  "-3 to -2",
+                                  "-2 to -1",
+                                  "-1 to 0",
+                                  "0 to 1",
+                                  "1 to 2",
+                                  "2 to 3",
+                                  "3 to 4",
+                                  "> 4")))
+
+PC_parameter_map=tm_shape(PC_params)+
+  tm_polygons(col='colors',
+              palette = tmaptools::get_brewer_pal("PRGn",n=10),
+              title="Probability of Connectivity")+
+  tm_layout(legend.title.size=1.5,
+            main.title = "A",
+            legend.text.size = .9)
+
+#####
+# Generate ECA GWR map:
+#####
+ECA_params = mean_params %>%
+  filter(index=="ECA_double") %>%
+  mutate(colors = case_when(mn_params < -.000015 ~ "< -1.5e-5",
+                            mn_params >= -.000015 &
+                              mn_params < -.0000075 ~ "-1.5e-5 to -7.5e-6",
+                            mn_params >= -.0000075 &
+                              mn_params < 0 ~ "-7.5e-6 to 0",
+                            mn_params >= 0 &
+                              mn_params < .0000075 ~ "0 to 7.5e-6",
+                            mn_params >= .0000075 &
+                              mn_params < .000015 ~ "7.5e-6 to 1.5e-5",
+                            mn_params >= .000015 &
+                              mn_params < .0000225 ~ "1.5e-5 to 2.25e-5",
+                            mn_params >= .0000225 &
+                              mn_params < .00003 ~ "2.25e-5 to 3.0e-5",
+                            mn_params >= .00003 ~ "> 3.0e-5")) %>%
+  mutate(colors = factor(colors,
+                         levels=c("< -1.5e-5",
+                                  "-1.5e-5 to -7.5e-6",
+                                  "-7.5e-6 to 0",
+                                  "0 to 7.5e-6",
+                                  "7.5e-6 to 1.5e-5",
+                                  "1.5e-5 to 2.25e-5",
+                                  "2.25e-5 to 3.0e-5",
+                                  "> 3.0e-5")))
+
+ECA_parameter_map=tm_shape(ECA_params)+
+  tm_polygons(col='colors',
+              palette=tmaptools::get_brewer_pal("PRGn"),
+              title="Equivalent Connected Area")+
+  tm_layout(main.title="B",
+            legend.title.size=1.5,
+            legend.text.size = .9)
+
+#####
+# Generate average deer take density map:
+#####
+avg_deer_take = spatial_reg_dat %>%
+  group_by(UNIT) %>%
+  summarize(mn_take_density = mean(Total/area,na.rm=T))
+
+Deer_take_map=tm_shape(avg_deer_take)+
+  tm_polygons(col='mn_take_density',
+              palette=tmaptools::get_brewer_pal("Greens",n=5),
+              title="Average yearly deer\ntake per hectare")+
+  tm_layout(main.title="D",
+            legend.title.size = 1.5,
+            legend.text.size = .9)
+
+#####
+# Compile all maps 
+#####
+Parameter_map = tmap_arrange(PC_parameter_map,ECA_parameter_map,
+                             custom_parameter_map,Deer_take_map,ncol=2)
+
+#####
+# Save map:
+#####
+tmap_save(Parameter_map,
+          filename=paste0(getwd(),'/Figures/Parameter_maps.jpeg'),
+          dpi = 300,
+          height=12)
