2aef4ec67e50c81061bf09dfca76ef3c00d5222f
coconn41 <collinoconnor3@gmail.com>
2024-05-21 15:08:49 -0400

    Computational efficiency improvement for clipping

--- Scripts/SCR/Calculate_buffer_thresholds.R
+++ Scripts/SCR/Calculate_buffer_thresholds.R
@@ -13,8 +13,9 @@ bufs = fin_poly[i,] %>%
 }
 
 clipped_polys <- foreach::foreach(i = 1:nrow(buffer_polys), .errorhandling = "remove", .combine = "rbind",.packages = c("sf", "dplyr")) %dopar% {
- 
-clp = st_intersection(buffer_polys[i,],fin_poly) %>%
+df = comps %>%
+  filter(row == i)
+clp = st_intersection(buffer_polys[i,],fin_poly[c(df$col),]) %>%
  dplyr::select(layer,layer.1,geometry) %>%
  mutate(j_area = st_area(.))
 
