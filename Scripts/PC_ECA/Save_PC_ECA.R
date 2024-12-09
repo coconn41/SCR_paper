4c9e1b9e47744f2c6250d5178a724e71a2621fc6
coconn41 <collinoconnor3@gmail.com>
2024-12-09 11:27:59 -0500

    Fix saving file path

--- Scripts/PC_ECA/Save_PC_ECA.R
+++ Scripts/PC_ECA/Save_PC_ECA.R
@@ -1,5 +1,5 @@
 
 if(buff_unbuff=="Buffered"){
-  write.csv(result_df,paste0(getwd(),"/SCR_paper/Data/Output_data/Buffered/Buffered_", x, "_.csv"))}
+  write.csv(result_df,paste0(getwd(),"/SCR_paper/Data/Output_data/PC_ECA/Buffered/Buffered_", x, "_.csv"))}
 if(buff_unbuff=="Unbuffered"){
-  write.csv(result_df,paste0(getwd(),"/SCR_paper/Data/Output_data/Unbuffered/Unbuffered_",x, "_.csv"))}
+  write.csv(result_df,paste0(getwd(),"/SCR_paper/Data/Output_data/PC_ECA/Unbuffered/Unbuffered_",x, "_.csv"))}
