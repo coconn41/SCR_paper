03037866dafb154c3bfa2b92e300c6a4ec4cd0a8
coconn41 <collinoconnor3@gmail.com>
2025-09-17 15:23:00 -0400

    Falsify

--- Scripts/SCR/Roadway_data_cleaning.R
+++ Scripts/SCR/Roadway_data_cleaning.R
@@ -18,6 +18,10 @@ remove(rwd,rwd2)
 #                                  ifelse(FUNCTIONAL %in% major_roadways,2,0))) %>%
 #   st_transform(.,crs=st_crs(template))
 
+roadway$empty = st_is_empty(roadway)
+roadway = roadway %>% 
+  filter(empty == F)
+
 road_vect = terra::vect(as(roadwaydat,"Spatial"))
 road_pix  = terra::rasterize(x = road_vect,
                              y = LCcrop,
