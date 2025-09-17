f31b47b091bcba8280094fbadf6abc0da27feca7
coconn41 <collinoconnor3@gmail.com>
2025-09-17 15:11:15 -0400

    vect

--- Scripts/SCR/Alpine_ecozone_cleaning.R
+++ Scripts/SCR/Alpine_ecozone_cleaning.R
@@ -3,7 +3,7 @@ alpine_regions = read_sf(paste0(getwd(),"/Data/Input_data/ny_eco_14/ny_eco_l4.sh
   mutate(US_L4CODE=1000) %>%
   st_transform(crs=32618)
 
-alpine_pix = terra::rasterize(x = alpine_regions,
+alpine_pix = terra::rasterize(x = vect(alpine_regions),
                               y = LCcrop,
                               field = "US_L4CODE",
                               background = 0)
